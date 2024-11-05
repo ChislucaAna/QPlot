d8db55077dabbe3a5911a86929abb13e96d760a6
Nunu <ana.chisluca.code@gmail.com>
2024-11-05 21:01:37 +0200

    Refined project structure by organizing views in separate files

--- visualizer_site/visualizerapp/forms.py
+++ visualizer_site/visualizerapp/forms.py
@@ -6,7 +6,7 @@ class PointForm(forms.Form):
     x = forms.FloatField(label='X Coordinate')
     y = forms.FloatField(label='Y Coordinate')
 
-class TriunghiForm(forms.Form):
+class TriangleForm(forms.Form):
     A = forms.FloatField(label='Frist Point')
     B = forms.FloatField(label='Second Point')
     C = forms.FloatField(label='Third Point')
