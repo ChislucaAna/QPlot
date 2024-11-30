bdf57c4c76fad0002ae1513fd59332d7a215edb8
Nunu <ana.chisluca.code@gmail.com>
2024-11-30 09:25:42 +0200

    Add function button and refactoring plotter code

--- visualizerapp/forms.py
+++ visualizerapp/forms.py
@@ -2,12 +2,18 @@
 from django import forms
 from .models.Points import Punct
 
+
 class PointForm(forms.Form):
     x = forms.FloatField(label='X:',required=False)
     y = forms.FloatField(label='Y:',required=False)
+
 
 class LineForm(forms.Form):
     x1 = forms.FloatField(label='X1:',required=False)
     y1 = forms.FloatField(label='Y1:',required=False)
     x2 = forms.FloatField(label='X2:',required=False)
     y2 = forms.FloatField(label='Y2:',required=False)
+
+
+class FunctionForm(forms.Form):
+    function = forms.CharField(label='Enter function', max_length=100, required=False)
--- visualizerapp/views/plotter.py
+++ visualizerapp/views/plotter.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
 # Forms for creating geometric elements
-from visualizerapp.forms import PointForm, LineForm
+from visualizerapp.forms import *
 from visualizerapp.models.Points import Punct
 from visualizerapp.models.Lines import Line
 #serializers
@@ -15,24 +15,19 @@ class PlotView(View):
     def get(self, request, *args, **kwargs):
         #init session
         self.initialize_session(request)
-        print(request.session['line_info'])
-        #init forms
         point_form=PointForm()
         line_form=LineForm()
+        func_form=FunctionForm()
         #current session context for data load
         context_data = self.get_session_data(request)
-        context = self.create_context(request, point_form, line_form,context_data)
-        #plot with current context
-        request.session['plot_url']= generate_plot_url(self.get_session_data(request))
-        #update context post-plot
-        context_data = self.get_session_data(request)
-        context = self.create_context(request, point_form, line_form,context_data)
+        #plot
+        context=self.plot_and_update_context(request,point_form,line_form,func_form)
         return render(request, 'plotter.html', context)
 
     def post(self, request, *args, **kwargs):
-
-        point_form = PointForm(request.POST or None)
+        point_form=PointForm(request.POST or None)
         line_form = LineForm(request.POST or None)
+        func_form = FunctionForm(request.POST or None)
 
         context_data = self.get_session_data(request)
 
@@ -48,34 +43,54 @@ class PlotView(View):
         elif 'show_slope' in request.POST:
             request.session['show_slope'] = not context_data['show_slope']
         elif 'add_point' in request.POST:
-            if point_form.is_valid():
-                x = point_form.cleaned_data['x']
-                y = point_form.cleaned_data['y']
-                new_point = Punct(float(x),float(y))
-                serialized_point=PunctSerializer(new_point).data
-                context_data['points'].append(serialized_point)
-                request.session['points'] = context_data['points']
-        elif'add_line' in request.POST:
-            if line_form.is_valid():
-                x1= line_form.cleaned_data['x1']
-                y1 = line_form.cleaned_data['y1']
-                x2 = line_form.cleaned_data['x2']
-                y2 = line_form.cleaned_data['y2']
-                p1 = Punct(float(x1),float(y1))
-                p2 = Punct(float(x2),float(y2))
-                new_line = Line(p1, p2)
-                serialized_line = LineSerializer(new_line).data
-                context_data['lines'].append(serialized_line) 
-                request.session['lines'] = context_data['lines']  
+            self.add_point(request,point_form,context_data)
+        elif 'add_line' in request.POST:
+            self.add_line(request,line_form,context_data)
+        elif 'add_function' in request.POST:
+            self.add_function(request,func_form,context_data)
+
         request.session.modified = True
-        #plot with current context
-        request.session['plot_url']= generate_plot_url(self.get_session_data(request))
-        #update context post-plot
-        context_data=self.get_session_data(request)
-        context = self.create_context(request, point_form, line_form, context_data)
+        context = self.plot_and_update_context(request, point_form, line_form,func_form)
         return render(request, 'plotter.html', context)
 
-    def create_context(self, request, point_form, line_form, context_data):
+    def add_point(self,request,point_form,context_data):
+        if point_form.is_valid():
+            x = point_form.cleaned_data['x']
+            y = point_form.cleaned_data['y']
+            new_point = Punct(float(x), float(y))
+            serialized_point = PunctSerializer(new_point).data
+            context_data['points'].append(serialized_point)
+            request.session['points'] = context_data['points']
+
+    def add_line(self, request, line_form, context_data):
+        if line_form.is_valid():
+            x1 = line_form.cleaned_data['x1']
+            y1 = line_form.cleaned_data['y1']
+            x2 = line_form.cleaned_data['x2']
+            y2 = line_form.cleaned_data['y2']
+            p1 = Punct(float(x1), float(y1))
+            p2 = Punct(float(x2), float(y2))
+            new_line = Line(p1, p2)
+            serialized_line = LineSerializer(new_line).data
+            context_data['lines'].append(serialized_line)
+            request.session['lines'] = context_data['lines']
+
+    def add_function(self, request, func_form, context_data):
+        if func_form.is_valid():
+            func = func_form.cleaned_data['function']
+            context_data['functions'].append(func)
+            request.session['functions'] = context_data['functions']
+        else:
+            print("invalid form")
+
+    def plot_and_update_context(self, request, point_form, line_form, func_form):
+        request.session['plot_url'] = generate_plot_url(self.get_session_data(request))
+        # update context post-plot
+        context_data = self.get_session_data(request)
+        context = self.create_context(request, point_form, line_form,func_form, context_data)
+        return context
+
+    def create_context(self, request, point_form, line_form,func_form, context_data):
         return {
             'connect_points': context_data['connect_points'],
             'show_middles': context_data['show_middles'],
@@ -84,10 +99,12 @@ class PlotView(View):
             'show_slope': context_data['show_slope'],
             'line_form': line_form,
             'point_form': point_form,
+            'func_form':func_form,
             'plot_url': context_data['plot_url'],
             'points': context_data['points'],
             'lines': context_data['lines'],
             'line_info': context_data['line_info'],
+            'functions': context_data['functions']
         }
     
     def initialize_session(self, request):
@@ -95,10 +112,11 @@ class PlotView(View):
             request.session['points'] = []
         if 'lines' not in request.session:
             request.session['lines'] = []
+        if 'functions' not in request.session:
+            request.session['functions'] = []
         if 'line_info' not in request.session:
             request.session['line_info'] = {}
 
-    
     def get_session_data(self, request):
         return {
             'connect_points': request.session.get('connect_points'),
@@ -108,6 +126,7 @@ class PlotView(View):
             'show_slope': request.session.get('show_slope'),
             'points': request.session.get('points', []),
             'lines': request.session.get('lines', []),
+            'functions':request.session.get('functions',[]),
             'plot_url': request.session.get('plot_url'),
             'line_info': request.session.get('line_info',{})
         }
