a77085da7a7c18bb9200b441cd90833c1b75c51c
Nunu <ana.chisluca.code@gmail.com>
2024-12-01 17:23:21 +0200

    Minor frontend imporvements for auth system and dashboard

--- visualizerapp/models/project_context.py
+++ visualizerapp/models/project_context.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class ProjectContext(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     context_data = models.JSONField()  # Store session data as JSON
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Project Context for {self.user}"
